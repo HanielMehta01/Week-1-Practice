0b60e80917be738ae8b45159f950ed767b3d3713
HanielMehta01 <hanielmehta01@gmail.com>
2026-05-13 21:38:44 +0530

    Practice-5 of Week1 of CS-50

--- Practice-4.cpp
+++ Practice-4.cpp
@@ -5,6 +5,6 @@ int main()
     int a, b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
-    cout << "The area of a rectangle with sides  " << a << " and " << b << "is" << a * b << endl;
+    cout << "The area of a rectangle with sides  " << a << " and " << b << " is " << a * b << endl;
     return 0;
 }
