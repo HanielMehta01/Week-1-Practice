64c4af75b1aca7489e48df43d5147fd4fa104b23
HanielMehta01 <hanielmehta01@gmail.com>
2026-05-13 22:06:26 +0530

    Practice-6 of cs-50 week1

--- Practice-6.cpp
+++ Practice-6.cpp
@@ -7,15 +7,15 @@ int main()
     cin >> a;
     if (a > 0)
     {
-        cout << "The number" << a << "is positive. " << endl;
+        cout << "The number " << a << " is positive. " << endl;
     }
     else if (a < 0)
     {
-        cout << "The number" << a << "is negative. " << endl;
+        cout << "The number " << a << " is negative. " << endl;
     }
     else
     {
-        cout << "The number" << a << "is neither positive nor negative it is zero. " << endl;
+        cout << "The number " << a << " is neither positive nor negative it is zero. " << endl;
     }
     return 0;
 }
