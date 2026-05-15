7587e62b18cecff8b5adf7bf2acbbfff40e79ea0
HanielMehta01 <hanielmehta01@gmail.com>
2026-05-15 12:33:06 +0530

    Edited

--- Practice-13.cpp
+++ Practice-13.cpp
@@ -5,7 +5,7 @@ int main()
     int a;
     cout << "Enter your selected number top find its multiplication table:";
     cin >> a;
-    cout << "Multiplication table of " << a << "is : " << endl;
+    cout << "Multiplication table of " << a << " is : " << endl;
     for (int i = 1; i <= 10; i++)
     {
         cout << i << "X" << a << "=" << i * a << endl;
